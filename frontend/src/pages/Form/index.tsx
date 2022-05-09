75bb6eb689e323475d9d01685020dd5c14d983d1
Robson Salaberry <robsalab@yahoo.com.br>
2022-05-09 16:30:52 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form() {
+
+    return (
+        <h1>Form page</h1>
+    );
+
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing() {
+
+    return (
+        <h1>List page</h1>
+    );
+
+}
+
+export default Listing;
